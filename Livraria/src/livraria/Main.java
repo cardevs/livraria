21662b1c61a5852ccdda78c420e411a12691aa30
Geraldo Quende <adolfoquende@hotmail.com>
2020-05-09 18:29:48 +0100

    Base Version

---
+++ Livraria/src/livraria/Main.java
@@ -0,0 +1,18 @@
+package livraria;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author CarneiroDev
+ */
+public class Main {
+   
+    public static void main(String[] args) {
+    
+        JOptionPane.showMessageDialog(null, "We are CarneiroDev!!!!!");
+        
+        
+    }
+    
+}
